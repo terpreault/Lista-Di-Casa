4c37d813472f67c97f65c25e844064f4582b2ad0
terpreault <terpreault@yahoo.com>
2026-09-23 22:16:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "casami-v8";
+const CACHE = "casami-v8-1-pop";
 const APP_SHELL = ["./", "./index.html", "./style.css", "./app.js", "./config.js", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
